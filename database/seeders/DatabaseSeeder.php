0af5598b2c3c5232238f9133355c8514043acc61
soulmax123 <soulmax123@gmail.com>
2026-03-14 22:58:49 -0400

    add database and migrate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        \App\Models\User::factory()->create([
+        'name' => 'Admin User',
+        'email' => 'admin@comp3385.com',
+        ]);
+
+
     }
 }
